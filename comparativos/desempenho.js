5eaf9e5b5bdd33110964509e83748f6e7d784a69
Leonardo Braz <lhleonardo05@gmail.com>
2021-10-11 14:16:35 -0300

    Padronização com prettier

--- comparativos/desempenho.js
+++ comparativos/desempenho.js
@@ -1,81 +1,85 @@
-const { v4: uuid } = require("uuid")
-const fs = require("fs").promises
+const { v4: uuid } = require("uuid");
+const fs = require("fs").promises;
 
-const axios = require("axios")
+const axios = require("axios");
 
 const client = axios.create({
-    baseURL: "http://172.21.66.232:3333"
-})
+  baseURL: "http://172.21.66.232:3333",
+});
 
-const NUM_EXECUTIONS = 1000
+const NUM_EXECUTIONS = 1000;
+const tipoImplementacao = "ddd";
 
 const requests = [
-    {
-        name: "Criar conta",
-        output: "./resultados/criar-conta-ddd.txt",
-        execute: async (index) => {
-            await client.post("/users", {
-                name: `Nome de teste ${index}`,
-                email: `${uuid().replace("-", "").replace(".", "")}@mail.com`,
-                password: '123456789',
-                confirmPassword: '123456789'
-            });
-        }
+  {
+    name: "Criar conta",
+    output: `./resultados/criar-conta-${tipoImplementacao}.txt`,
+    execute: async (index) => {
+      await client.post("/users", {
+        name: `Nome de teste ${index}`,
+        email: `${uuid().replace("-", "").replace(".", "")}@mail.com`,
+        password: "123456789",
+        confirmPassword: "123456789",
+      });
     },
-    {
-        name: "Agendar um atendimento",
-        output: "./resultados/criar-agendamento-ddd.txt",
-        execute: async (index) => {
-            const appointmentDate = new Date();
-            appointmentDate.setDate(appointmentDate.getDate() + index);
+  },
+  {
+    name: "Agendar um atendimento",
+    output: `./resultados/criar-agendamento-${tipoImplementacao}.txt`,
+    execute: async (index) => {
+      const appointmentDate = new Date();
+      appointmentDate.setDate(appointmentDate.getDate() + index);
 
-            const userToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MzM5NzEzMTQsImV4cCI6MTYzNDA1NzcxNCwic3ViIjoiZTEyOTU0ZDktNDc0ZS00NGUxLWE0NGYtZjg1MDM0ZTg5MzA1In0.yxdBzkxGNPw2XbbADw7zpaTUjyVZ8rfDNmvl81u0pZw"
-            const providerId = "00ce9d7c-ec9a-438c-a7a6-74696d2e2f8d"
+      const userToken =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MzM5NzEzMTQsImV4cCI6MTYzNDA1NzcxNCwic3ViIjoiZTEyOTU0ZDktNDc0ZS00NGUxLWE0NGYtZjg1MDM0ZTg5MzA1In0.yxdBzkxGNPw2XbbADw7zpaTUjyVZ8rfDNmvl81u0pZw";
+      const providerId = "00ce9d7c-ec9a-438c-a7a6-74696d2e2f8d";
 
-            await client.post("/appointments", { providerId, date: appointmentDate }, { headers: { "Authorization": `Bearer ${userToken}` } })
-        }
+      await client.post(
+        "/appointments",
+        { providerId, date: appointmentDate },
+        { headers: { Authorization: `Bearer ${userToken}` } }
+      );
     },
-]
+  },
+];
 
 async function executeRequest(request) {
-    const executionTimes = []
+  const executionTimes = [];
 
-    for (let i = 0; i < NUM_EXECUTIONS; i++) {
-        const startTime = performance.now();
+  for (let i = 0; i < NUM_EXECUTIONS; i++) {
+    const startTime = performance.now();
 
-        await request.execute(i + 1);
+    await request.execute(i + 1);
 
-        const endTime = performance.now();
+    const endTime = performance.now();
 
-        const runningTime = (endTime - startTime) / 1000;
+    const runningTime = (endTime - startTime) / 1000;
 
-        executionTimes.push(runningTime);
-    }
+    executionTimes.push(runningTime);
+  }
 
-    return executionTimes;
+  return executionTimes;
 }
 
 async function saveResults(output, times) {
-    let content = "";
+  let content = "";
 
-    times.forEach(time => content += time + "\n");
+  times.forEach((time) => (content += time + "\n"));
 
-    await fs.writeFile(output, content);
+  await fs.writeFile(output, content);
 }
-
 
 async function main() {
-    for (const request of requests) {
-        console.log(`Executando: ${request.name}`)
-        console.time(request.name);
-    
-        const runningTimes = await executeRequest(request);
-    
-        await saveResults(request.output, runningTimes);
-    
-        console.timeEnd(request.name);
-    }
+  for (const request of requests) {
+    console.log(`Executando: ${request.name}`);
+    console.time(request.name);
+
+    const runningTimes = await executeRequest(request);
+
+    await saveResults(request.output, runningTimes);
+
+    console.timeEnd(request.name);
+  }
 }
 
-
-main()
+main();
