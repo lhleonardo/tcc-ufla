54971c435e6d654fe01014f2c70788319083cf37
Leonardo Braz <lhleonardo05@gmail.com>
2021-10-28 09:44:07 -0300

    adiciona tratativas de erro

--- comparativos/desempenho-online.js
+++ comparativos/desempenho-online.js
@@ -5,7 +5,7 @@ const client = axios.create({
   baseURL: "http://157.230.0.64:3333",
 });
 
-const NUM_EXECUTIONS = 300;
+const NUM_EXECUTIONS = 2000;
 
 let loggedUserToken = undefined;
 
@@ -23,69 +23,88 @@ async function generateSession(email, password) {
 
 async function generateUsers() {
   for (let i = 0; i < NUM_EXECUTIONS; i++) {
-    const name = uuid().replace("-", "").replace(".", "");
-    const createdUser = await client.post("/users", {
-      name: `Name ${name}`,
-      email: `${name}@mail.com`,
-      password: "123456789",
-      confirmPassword: "123456789",
-    });
+    try {
+      const name = uuid().replace("-", "").replace(".", "");
+      const createdUser = await client.post("/users", {
+        name: `Name ${name}`,
+        email: `${name}@mail.com`,
+        password: "123456789",
+        confirmPassword: "123456789",
+      });
 
-    //autentica e deixa o token guardado para usuário ou provedor, mas ambos não podem ser o mesmo usuário.
-    if (!loggedUserToken) {
-      loggedUserToken = await generateSession(`${name}@mail.com`, "123456789");
-      continue;
-    }
+      //autentica e deixa o token guardado para usuário ou provedor, mas ambos não podem ser o mesmo usuário.
+      if (!loggedUserToken) {
+        loggedUserToken = await generateSession(
+          `${name}@mail.com`,
+          "123456789"
+        );
+        continue;
+      }
 
-    if (!providerUserToken) {
-      providerId = createdUser.data.id;
-      providerUserToken = await generateSession(
-        `${name}@mail.com`,
-        "123456789"
-      );
-      continue;
+      if (!providerUserToken) {
+        providerId = createdUser.data.id;
+        providerUserToken = await generateSession(
+          `${name}@mail.com`,
+          "123456789"
+        );
+        continue;
+      }
+    } catch (err) {
+      console.log("Deu um erro ao criar uma conta");
     }
   }
 }
 
 async function generateAppointments() {
   for (let i = 0; i < NUM_EXECUTIONS; i++) {
-    const appointmentDate = new Date();
-    appointmentDate.setDate(appointmentDate.getDate() + (i + 1));
-    appointmentDate.setHours(10);
-    appointmentDate.setMinutes(0);
-    appointmentDate.setSeconds(0);
-    appointmentDate.setMilliseconds(0);
+    try {
+      const appointmentDate = new Date();
+      appointmentDate.setDate(appointmentDate.getDate() + (i + 1));
+      appointmentDate.setHours(10);
+      appointmentDate.setMinutes(0);
+      appointmentDate.setSeconds(0);
+      appointmentDate.setMilliseconds(0);
 
-    await client.post(
-      "/appointments",
-      { providerId, date: appointmentDate },
-      { headers: { Authorization: `Bearer ${loggedUserToken}` } }
-    );
+      await client.post(
+        "/appointments",
+        { providerId, date: appointmentDate },
+        { headers: { Authorization: `Bearer ${loggedUserToken}` } }
+      );
+    } catch (err) {
+      console.log("Deu um erro ao marcar um agendamento");
+    }
   }
 }
 
 async function getProviders() {
   for (let i = 0; i < NUM_EXECUTIONS; i++) {
-    await client.get("/providers", {
-      headers: { Authorization: `Bearer ${loggedUserToken}` },
-    });
+    try {
+      await client.get("/providers", {
+        headers: { Authorization: `Bearer ${loggedUserToken}` },
+      });
+    } catch (err) {
+      console.log("Deu um erro ao processar um prestador");
+    }
   }
 }
 
 async function getSchedules() {
   for (let i = 0; i < NUM_EXECUTIONS; i++) {
-    const dataListagem = new Date();
-    dataListagem.setDate(dataListagem.getDate() + i);
+    try {
+      const dataListagem = new Date();
+      dataListagem.setDate(dataListagem.getDate() + i);
 
-    await client.get("/appointments/me", {
-      headers: { Authorization: `Bearer ${providerUserToken}` },
-      params: {
-        year: dataListagem.getFullYear(),
-        month: dataListagem.getMonth() + 1,
-        day: dataListagem.getDate(),
-      },
-    });
+      await client.get("/appointments/me", {
+        headers: { Authorization: `Bearer ${providerUserToken}` },
+        params: {
+          year: dataListagem.getFullYear(),
+          month: dataListagem.getMonth() + 1,
+          day: dataListagem.getDate(),
+        },
+      });
+    } catch (err) {
+      console.log("Deu um erro ao ver um agendamento");
+    }
   }
 }
 
